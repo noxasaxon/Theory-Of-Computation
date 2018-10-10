3b6e050b91b24b31879f5b8e1cb2c8a1e8b107c6
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-10-09 17:48:11 -0700

    phone.py complete

--- projects/state-mach-regex/phone/phone.py
+++ projects/state-mach-regex/phone/phone.py
@@ -4,18 +4,34 @@ import re # module for processing regular expressions https://docs.python.org/3/
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # TODO Define your regex
+entirePhoneRegex = '^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$'
 
-
+removeNonIntRegex = '\d'
 while line != "exit":
     # TODO Find matches
+    matchObj = re.search(entirePhoneRegex, line)
+    
+    
+    if not matchObj: 
+        # TODO If no match found, print that no number was found
+        print('No valid number found')
+    else:
+        #match found
+        # TODO Else, break number up into area code, prefix, and suffix
+        myMatch = matchObj.group(0)
+        print(myMatch)
+        phoneNum = re.sub('[^0-9]','' ,myMatch)
+        print(phoneNum)
 
-    
-    # TODO If no match found, print that no number was found
-   
-   
-    
-    # TODO Else, break number up into area code, prefix, and suffic
-    
+        #get area code
+        areaCode = phoneNum[:3]
+        #get prefix
+        prefix = phoneNum[3:6]
+        #get suffix
+        suffix = phoneNum[6:]
+        
+        print(f'Area Code: {areaCode}, Prefix: {prefix}, Suffix: {suffix}')
+
     
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
